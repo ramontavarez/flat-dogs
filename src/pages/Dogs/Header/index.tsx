fa265f48a297caf1d264d6ee01180420f30639d1
ramontavarez <ramonbardic@gmail.com>
2023-04-27 00:21:17 -0300

    chore: add subtitle

--- src/pages/Dogs/Header/index.tsx
+++ src/pages/Dogs/Header/index.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className="flex flex-col items-center text-white mt-8 gap-4">
             <h1 className="text-5xl font-bold">FLAT DOGS</h1>
+            <span className="font-normal">Cachorros gerados por inteligência artificial (midjourney).</span>
             <img src={banner} />
         </header>
     )
